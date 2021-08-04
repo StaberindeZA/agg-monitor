4866eeae95f59d3177bf44dff89c6bf0508aa80d
Reino Muhl <reino.muhl@gmail.com>
2021-08-04 18:30:30 -0400

    Minor changes

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,10 +1,10 @@
-import {Controller, Get, Post} from '@overnightjs/core';
+import {Controller, Delete, Get, Post} from '@overnightjs/core';
 import {Request, Response} from 'express';
 import {User} from '../models/users';
 
 @Controller('users')
 export class UsersController {
-  @Get('')
+  @Get()
   private async list(req: Request, res: Response) {
     const users = await User.find().limit(10);
     res.json(users);
@@ -16,5 +16,15 @@ export class UsersController {
     const user = new User(body);
     await user.save();
     res.json(user);
+  }
+
+  @Delete(':id')
+  private async delete(req: Request, res: Response) {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (user) {
+      res.status(204).send();
+    } else {
+      res.status(404).send('Error');
+    }
   }
 }
